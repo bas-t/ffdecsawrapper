3596252aa2fec0f4c11378ad001e2b8250ee61bb
leslie <devnull@localhost>
2007-12-27 22:01:12 +0100

    release 0.8.4

--- systems/nagra/nagra2-4101.c
+++ systems/nagra/nagra2-4101.c
@@ -19,14 +19,22 @@
 
 // -- cN2Prov4101 ----------------------------------------------------------------
 
-class cN2Prov4101 : public cN2Prov {
+class cN2Prov4101 : public cN2Prov, public cN2Emu {
 protected:
+  virtual void WriteHandler(unsigned char seg, unsigned short ea, unsigned char &op);
 public:
-  cN2Prov4101(int Id, int Flags):cN2Prov(Id,Flags) {}
+  cN2Prov4101(int Id, int Flags);
   virtual bool PostProcAU(int id, unsigned char *data);
+  virtual int ProcessBx(unsigned char *data, int len, int pos);
   };
 
-static cN2ProvLinkReg<cN2Prov4101,0x4101,N2FLAG_POSTAU> staticPL4101;
+static cN2ProvLinkReg<cN2Prov4101,0x4101,N2FLAG_POSTAU|N2FLAG_Bx> staticPL4101;
+
+cN2Prov4101::cN2Prov4101(int Id, int Flags)
+:cN2Prov(Id,Flags)
+{
+  hasWriteHandler=true;
+}
 
 bool cN2Prov4101::PostProcAU(int id, unsigned char *data)
 {
@@ -47,13 +55,38 @@ bool cN2Prov4101::PostProcAU(int id, unsigned char *data)
   return true;
 }
 
-// -- cN2Prov7101 ----------------------------------------------------------------
+int cN2Prov4101::ProcessBx(unsigned char *data, int len, int pos)
+{
+  if(Init(id,110)) {
+    SetMem(0x80,data,len);
+    SetPc(0x80+pos);
+    SetSp(0x0FFF,0x0FE0);
+    ClearBreakpoints();
+    AddBreakpoint(0xACDD);
+    AddBreakpoint(0x9BDD);
+    while(!Run(5000)) {
+      switch(GetPc()) {
+        case 0xACDD:
+          return -1;
+        case 0x9BDD:
+          GetMem(0x80,data,len);
+          return a;
+        }
+      }
+    }
+  return -1;
+}
 
-/*
-class cN2Prov7101 : public cN2Prov4101 {
-public:
-  cN2Prov7101(int Id, int Flags):cN2Prov4101(Id,Flags) {}
-  };
-*/
+void cN2Prov4101::WriteHandler(unsigned char seg, unsigned short ea, unsigned char &op)
+{
+  if(cr==0x00) {
+    if(ea==0x0a || ea==0x12 || ea==0x16) {
+      unsigned char old=Get(ea);
+      if(old&2) op=(old&~0x02) | (op&0x02);
+      }
+    }
+}
+
+// -- cN2Prov7101 ----------------------------------------------------------------
 
 static cN2ProvLinkReg<cN2Prov4101,0x7101,N2FLAG_POSTAU> staticPL7101;
---
+++ systems/sc-videoguard2/sc-videoguard2.mk
@@ -0,0 +1,5 @@
+#
+# Smartcard VideoGuard2
+#
+TARGET = sc_videoguard2
+OBJS   = sc-videoguard2.o
