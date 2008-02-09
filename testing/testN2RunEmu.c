b7fdca171be754c039e804a007738d04af98ef01
leslie <devnull@localhost>
2008-02-09 13:53:02 +0100

    update testing

--- testing/testN2RunEmu.c
+++ testing/testN2RunEmu.c
@@ -20,11 +20,11 @@ int main(int argc, char *argv[])
   LogAll();
   if(argc<=4 || strcasecmp(argv[4],"LOG"))
     cLogging::SetModuleOption(LCLASS(L_SYS,L_SYS_DISASM),false);
-  unsigned char data[1024];
+  unsigned char data[4096];
   unsigned int len=ReadRaw(argv[3],data,sizeof(data));
   int id=strtol(argv[2],0,0);
   cN2Prov *emmP=cN2Providers::GetProv(id,N2FLAG_NONE);
   HEXDUMP(L_SYS_EMU,data,len,"Input");
-  if(emmP->RunEmu(data,len,0x90,0x90,0x00,0x00,0x300)>=0)
-    HEXDUMP(L_SYS_EMU,data,0x300,"Output");
+  if(emmP->RunEmu(data,len,0x90,0x90,0x00,0x00,0x460)>=0)
+    HEXDUMP(L_SYS_EMU,data,0x460,"Output");
 }
