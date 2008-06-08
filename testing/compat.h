365c2e9bf201c243b970b3649d41416128d9f285
leslie <unknown>
2008-06-08 12:16:48 +0800

    update testing

--- testing/compat.h
+++ testing/compat.h
@@ -2,5 +2,6 @@
 bool DllsLoad(const char *libdir);
 void InitAll(const char *cfgdir);
 void LogAll(void);
+void LogNone(void);
 void SDump(const unsigned char *buffer, int n);
 int ReadRaw(const char *name, unsigned char *buff, int maxlen);
