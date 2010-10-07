5739c52c8aa1287d67a093e58de6ef635573f86e
leslie <unknown>
2010-10-07 17:51:15 +0200

    added fr4nK10's NDS-EMM parse patch

--- systems/sc-videoguard2/sc-videoguard2.c
+++ systems/sc-videoguard2/sc-videoguard2.c
@@ -674,7 +674,7 @@ bool cSmartCardVideoGuard2::Update(int pid, int caid, const unsigned char *data)
 {
   static unsigned char ins42[5] = { 0xD1,0x42,0x00,0x00,0xFF }; 
   if(MatchEMM(data)) {
-    const unsigned char *payloaddata=cParseNDS::PayloadStart(data); //points to 02 xx yy
+    const unsigned char *payloaddata=cParseNDS::PayloadStart(data,cardID); //points to 02 xx yy
     int lenEMM;
     switch(payloaddata[0]) {
       case 2:
