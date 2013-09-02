d9a3ccc1caadc33889807772bdbdc4964df9e034
Tycho Lursen <tycholursen@gmail.com>
2013-09-02 11:49:23 +0200

    Move handling of dvbdev.h into configure

--- contrib/ffdecsawrapper/dvbloopback/module/config_dvb.pl
+++ contrib/ffdecsawrapper/dvbloopback/module/config_dvb.pl
@@ -12,18 +12,14 @@ sub test_dvb_adapter(\@) {
     $vars .= "\"$i\" ";
   }
   unlink("dvbdevwrap.h");
-  unlink("dvbdev.h");
-  unlink("config-dvb/dvbdev.h");
   $cmd = "cd config-dvb && make $vars" . ($debug ? "" : "2>/dev/null 1>/dev/null");
   print "$cmd\n" if($debug);
 
   system("ln -sf chkdvb-2.6.v4l.c config-dvb/chkdvb.c");
-  system("ln -sf ../dvbdev-2.6.38.h config-dvb/dvbdev.h");
    print "Using canned header\n";
     if(system("$cmd") == 0) {
     `echo "DVB_DEFINE_MOD_OPT_ADAPTER_NR(adapter_nr);" >> dvbdevwrap.h`; 
     `echo "#define wrap_dvb_reg_adapter(a, b, c) dvb_register_adapter(a, b, c, &dvblb_basedev->dev, adapter_nr)" >> dvbdevwrap.h`; 
-     system("ln -sf dvbdev-2.6.38.h dvbdev.h"); 
     return 0;
   }
 }
