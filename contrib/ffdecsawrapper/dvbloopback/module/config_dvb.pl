3245b3b379bd6794dc0c642ed060d75e0f84d8f4
Tycho Lursen <tycholursen@gmail.com>
2013-09-02 11:51:03 +0200

    We are allways using the right header

--- contrib/ffdecsawrapper/dvbloopback/module/config_dvb.pl
+++ contrib/ffdecsawrapper/dvbloopback/module/config_dvb.pl
@@ -16,7 +16,6 @@ sub test_dvb_adapter(\@) {
   print "$cmd\n" if($debug);
 
   system("ln -sf chkdvb-2.6.v4l.c config-dvb/chkdvb.c");
-   print "Using canned header\n";
     if(system("$cmd") == 0) {
     `echo "DVB_DEFINE_MOD_OPT_ADAPTER_NR(adapter_nr);" >> dvbdevwrap.h`; 
     `echo "#define wrap_dvb_reg_adapter(a, b, c) dvb_register_adapter(a, b, c, &dvblb_basedev->dev, adapter_nr)" >> dvbdevwrap.h`; 
