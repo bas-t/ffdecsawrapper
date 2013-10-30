d7a5b166457e02d3ddabe9a2b413611a699a3aac
Tycho Lursen <tycholursen@gmail.com>
2013-10-30 22:59:43 +0100

    Bump version to 1.1.5

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION = 1.1.4
+VERSION = 1.1.5
 
 $(shell touch config.mak)
 include config.mak
--- sc/PLUGINS/src/version.h
+++ sc/PLUGINS/src/version.h
@@ -20,7 +20,7 @@
 #ifndef ___VERSION_H
 #define ___VERSION_H
 
-#define SC_RELEASE "1.1.4"
+#define SC_RELEASE "1.1.5"
 
 extern const char *ScVersion;
 
